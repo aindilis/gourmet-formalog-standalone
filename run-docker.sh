5e146362fb06bb51aa45e51153433c96e26b6c13
Andrew John Dougherty <adougher9@gmail.com>
2021-08-10 20:50:47 -0400

    Got some of the way through Docker image installation, but doesn't work yet.

---
+++ run-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker build --rm  .
+docker run -e "USER=andrewdo" -e "GROUP=andrewdo" -e "USER_ID=1000" -e "GROUP_ID=1000" .
+
+# docker run --rm -v "$(pwd):/$pwd | slugify)" -w "/$(pwd | slugify)" -ti --entrypoint=aindilis-gourmet-formalog-standalone
+
+# docker run --rm -ti --entrypoint= aindilis-gourmet-formalog-standalone-2146d58a:1.0 bash
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
 
-if [[ $EUID -eq 0 ]]; then
-    echo "This script must be run as a regular user with sudo privileges"
-    exit 1
-fi
+apt-get install -y git unzip wget libstring-shellquote-perl sudo swi-prolog-nox
 
-sudo apt-get install -y cpanminus git unzip wget swi-prolog-nox
+su andrewdo -c /home/andrewdo/setup.pl
 
-sudo cpanm File::Basename
-sudo cpanm String::ShellQuote
+# if ! command -v git &> /dev/null
+# then
+#     echo "git could not be found"
+#     exit
+# fi
 
-if ! command -v git &> /dev/null
-then
-    echo "git could not be found"
-    exit
-fi
+# if ! command -v unzip &> /dev/null
+# then
+#     echo "unzip could not be found"
+#     exit
+# fi
 
-if ! command -v unzip &> /dev/null
-then
-    echo "unzip could not be found"
-    exit
-fi
+# if ! command -v tar &> /dev/null
+# then
+#     echo "tar could not be found"
+#     exit
+# fi
 
-if ! command -v tar &> /dev/null
-then
-    echo "tar could not be found"
-    exit
-fi
-
-if ! command -v wget &> /dev/null
-then
-    echo "wget could not be found"
-    exit
-fi
-
-./setup.pl
+# if ! command -v wget &> /dev/null
+# then
+#     echo "wget could not be found"
+#     exit
+# fi
